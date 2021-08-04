9495830f466080cc3529db32d7b04b01ae4333b9
George-Sucuzhanay <georgesucuzhanay1002gmail.com>
2021-08-04 00:46:59 +0000

    new viewSaved page and more

---
+++ public/js/viewPlans.js
@@ -0,0 +1,145 @@
+
+let userGlobalID = null;
+
+window.onload = event => {
+    // Firebase authentication goes here.
+    firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+        // Console log the user to confirm they are logged in
+        console.log("Logged in as: " + user.displayName);
+        const googleUserId = user.uid;
+        userGlobalID = googleUserId;
+        //recipeFeed();   
+        generateRecipeFeed();   
+    } else {
+      // If not logged in, navigate back to login page.
+      window.location = "index.html";
+    }
+  });
+};
+
+//for testing: apiKey = 26d5ee965b7041448718ba0f2475dc94
+//for deploy: apiKey = xxx 
+// function recipeFeed() {
+//     //will return 5078 recipe cards for browsing
+//     //
+//     const spoonacularURL = "https://api.spoonacular.com/recipes/random"
+
+//     const apiKey = "26d5ee965b7041448718ba0f2475dc94"
+
+//     const authorizedURL = spoonacularURL + "?apiKey=" + apiKey + "&number=100"
+
+//     fetch(authorizedURL)
+//     .then(response => {
+//         return response.json();
+//     })
+//     .then(myjson => {
+//         //THIS IS WHERE YOU HANDLE THE RESPONSE JSON
+//         console.log(myjson);
+//         storeRecipes(myjson)
+//     });
+// }
+
+//to avoid filling api request quota
+// function storeRecipes(myjson){
+//     console.log('entered')
+//     firebase
+//         .database()
+//         .ref(`recipes`)
+//         .push({
+//             JSONObj: myjson
+//         });
+//     const recipes = firebase.database().ref(`recipes`);
+//     recipes.on("value", snapshot => {
+//         const data = snapshot.val();
+//         console.log(data)
+//   });
+// };
+
+let globalRecipeData = null
+
+function generateRecipeFeed(){
+    const recipes = firebase.database().ref(`recipes`);
+    recipes.on("value", snapshot => {
+        const data = snapshot.val();
+        globalRecipeData = data;
+        let recipeArray = exploreData(globalRecipeData);
+        renderDataAsHTML(recipeArray);
+  });
+}
+
+function exploreData(data){
+    let jsonObj = null;
+    for(let item in data){
+        jsonObj = data[item].JSONObj.recipes;
+        //console.log(jsonObj);
+    };
+    // console.log(jsonObj[0])
+    // for(let item in jsonObj){
+    //     console.log(jsonObj[item])
+    // }
+    return jsonObj;
+}
+
+const createCard = (recipeData) => {
+
+    let id = recipeData.id;
+    let image = recipeData.image;
+    let title = recipeData.title;
+    let source = recipeData.spoonacularSourceUrl;
+    let likes = recipeData.aggregateLikes;
+
+    if(image===undefined){
+        image="https://media.istockphoto.com/photos/question-mark-made-of-corn-seeds-on-plate-picture-id467083203?k=6&m=467083203&s=612x612&w=0&h=pfMfAgrliETJB2cUsxQBIkSXNlAKT4gf4hEEz80r4Hw="
+    }    
+
+  return `
+        <div class="row">
+        <div class="col">
+            <img src="${image} alt="${title}" class="img-fluid">
+            <div class="card-body">
+                    <h4 class="card-title text-center">${title}</h4>
+                </div>
+        </div>
+        <div class="col">
+            <img src="${image} alt="${title}"  class="img-fluid">
+            <div class="card-body">
+                    <h4 class="card-title text-center">${title}</h4>
+                </div>
+        </div>
+        <div class="col">
+            <img src="${image} alt="${title}z" class="img-fluid">
+            <div class="card-body">
+                    <h4 class="card-title text-center">${title}</h4>
+                </div>
+        </div>
+        <div class="col">
+            <img src="${image} alt="${title}" class="img-fluid">
+            <div class="card-body">
+                 
+                    <h4 class="card-title text-center">${title}</h4>
+                </div>
+        </div>
+        <div class="col">
+            <img src="${image} alt="${title}" class="img-fluid">
+            <div class="card-body">
+                    <h4 class="card-title text-center">${title}</h4>
+                </div>
+        </div>
+    </div>
+            
+            `;
+}
+
+const renderDataAsHTML = (data) => {
+    //let cards = `<div class="card-deck"></div>`;
+    let cards = ""
+    for (let item in data) {
+        console.log(data[item])
+        cards+=createCard(data[item])
+  }
+  document.querySelector("#app").innerHTML = cards;
+};
+
+
+
