d8eb67c99675a714dd80dac3122aeb8bac9b2623
pineappleEnthusiast <saloni.a.modi@gmail.com>
2021-08-06 20:53:40 +0000

    viewPlans.js stuff

--- public/js/viewPlans.js
+++ public/js/viewPlans.js
@@ -38,7 +38,7 @@ function makePlan(){
                 const shuffled = arr.sort(() => 0.5 - Math.random());
                 let selected = shuffled.slice(0, 6);
                 const spoonacularURL = "https://api.spoonacular.com/recipes/informationBulk"
-                const apiKey = "c9a6277cf82249b9b64ccaeb744230f8"
+                const apiKey = "9a3b42061bbc447fb83d3ee7c6df67f7"
                 const authorizedURL = spoonacularURL + "?apiKey=" + apiKey + "&ids=" + selected.toString()
                 fetch(authorizedURL)
                 .then(response => {
@@ -49,42 +49,135 @@ function makePlan(){
                     //https://api.spoonacular.com/recipes/1082038/priceBreakdownWidget
                     //ingredient breakdown by ID returns HTML
                     //https://api.spoonacular.com/recipes/1082038/ingredientWidget
-                    console.log(myjson)
+                    //console.log(myjson)
                     let arr=[];
                     for(let item in myjson){
                     arr.push(myjson[item])
                     }
-                    console.log(arr)
-                    let ing = parseExtendedIngredients(arr,0)
-                    document.querySelector("#monFront").innerHTML=`<p>${arr[0].title}</p>`
+                    //console.log(arr)
+                    let moning = parseIngText(parseExtendedIngredients(arr[0]))
+                    document.querySelector("#monFront").innerHTML=`<p>${arr[0].title}</p><div>${moning}</div>`
 
-                    document.querySelector("#monBack").innerHTML=`<p>a</p>`
+                    let tuesing = parseIngText(parseExtendedIngredients(arr[1]))
+                    document.querySelector("#tuesFront").innerHTML=`<p>${arr[1].title}</p><div>${tuesing}</div>`
 
-                    document.querySelector("#tuesFront").innerHTML=`<p>${arr[1].title}</p>`
+                    let weding = parseIngText(parseExtendedIngredients(arr[2]))
+                    document.querySelector("#wedFront").innerHTML=`<p>${arr[2].title}</p><div>${weding}</div>`
 
-                    document.querySelector("#tuesBack").innerHTML=`<p>a</p>`
+                    let thursing = parseIngText(parseExtendedIngredients(arr[3]))
+                    document.querySelector("#thursFront").innerHTML=`<p>${arr[3].title}</p><div>${thursing}</div>`
 
-                    document.querySelector("#wedFront").innerHTML=`<p>${arr[2].title}</p>`
+                    let friing = parseIngText(parseExtendedIngredients(arr[4]))
+                    document.querySelector("#friFront").innerHTML=`<p>${arr[4].title}</p><div>${friing}</div>`
 
-                    document.querySelector("#wedBack").innerHTML=`<p>a</p>`
+                    let sating = parseIngText(parseExtendedIngredients(arr[5]))
+                    document.querySelector("#satFront").innerHTML=`<p>${arr[5].title}</p><div>${sating}</div>`
 
-                    document.querySelector("#thursFront").innerHTML=`<p>${arr[3].title}</p>`
-
-                    document.querySelector("#thursBack").innerHTML=`<p>a</p>`
-
-                    document.querySelector("#friFront").innerHTML=`<p>${arr[4].title}</p>`
-
-                    document.querySelector("#friBack").innerHTML=`<p>a</p>`
-
-                    document.querySelector("#satFront").innerHTML=`<p>${arr[5].title}</p>`
-
-                    document.querySelector("#satBack").innerHTML=`<p></p>`
                 });
+                widgetGenerator(selected)
             }
         }
     });
 }
 
-function parseExtendedIngredients(){
+function widgetGenerator(arr){
+    for(let item in arr){
+        const spoonacularURL = "https://api.spoonacular.com/recipes/" + arr[item] + "/nutritionWidget"
+        const apiKey = "9a3b42061bbc447fb83d3ee7c6df67f7"
+        const authorizedURL = spoonacularURL + "?apiKey=" + apiKey + "&defaultCss=true"
+            fetch(authorizedURL)
+            .then(response => {
+                return response.body;
+            })
+            .then(rb => {
+                const reader = rb.getReader();
+                return new ReadableStream({
+                    start(controller) {
+                    // The following function handles each data chunk
+                    function push() {
+                        // "done" is a Boolean and value a "Uint8Array"
+                        reader.read().then( ({done, value}) => {
+                        // If there is no more data to read
+                        if (done) {
+                            console.log('done', done);
+                            controller.close();
+                            return;
+                        }
+                        // Get the data and send it to the browser via the controller
+                        controller.enqueue(value);
+                        // Check chunks by logging to the console
+                        console.log(done, value);
+                        push();
+                        })
+                    }
+                    push();
+                    }
+                });
+                })
+                .then(stream => {
+                // Respond with our stream
+                return new Response(stream, { headers: { "Content-Type": "text/html" } }).text();
+                })
+                .then(result => {
+                // Do things with result
+                    console.log(result)
+                    document.getElementById(item).innerHTML=result;
+                });
+            }
+                }
 
+function parseExtendedIngredients(arr){
+    let list = []
+    let ext = arr.extendedIngredients
+    //console.log(ext)
+    for(let item in ext){
+        //console.log(ext[item])
+        //console.log(ext[item].aisle)
+        list.push({
+            "aisle":ext[item].aisle,
+            "name":ext[item].nameClean
+        })
+    }
+    //console.log(list)
+    return list
 }
+
+function parseIngText(arr){
+    let table=`<table id="t01" style="font-size:13px;font-family:Londrina Solid; text-align:center;">
+  <tr>
+    <th>Aisle</th>
+    <th>Ingredient</th> 
+  </tr>`
+    
+    if(arr.length>10){
+        arr=arr.slice(0,10)
+        viewMoreModal(arr)
+        //console.log("slicing")
+        for(let ing in arr){
+            table+=`<tr><td>${arr[ing].aisle}</td><td>${arr[ing].name}</td></tr>`
+        }
+        return table+`<tr><td>"..."</td><td>"..."</td></tr></table>`
+    }
+    else{
+        for(let ing in arr){
+            table+=`<tr><td>${arr[ing].aisle}</td><td>${arr[ing].name}</td></tr>`
+        }
+        return table+`</table>`
+    }
+}
+
+function viewMoreModal(arr){
+    //console.log("opening")    
+    extraStuff=document.querySelector("#extra")
+    let table=`<table id="t01" style="font-size:13px;font-family:Londrina Solid; text-align:center;">
+  <tr>
+    <th>Aisle</th>
+    <th>Ingredient</th> 
+  </tr>`
+    for(let ing in arr){
+        table+=`<tr><td>${arr[ing].aisle}</td><td>${arr[ing].name}</td></tr>`
+    }
+    table+=`</table>`
+    extra.innerHTML=table;
+}
+
